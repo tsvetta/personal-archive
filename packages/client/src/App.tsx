8bc22aa7b8ee14d7790b163ec1a3a34d6b6c850a
tsvetta <tsvetta@gmail.com>
2024-07-04 15:08:50 +0400

    Remove unnessessary code

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,3 @@
-import { useQuery, gql } from '@apollo/client';
 import { Routes, Route } from 'react-router-dom';
 
 import './index.css';
@@ -10,41 +9,7 @@ import MainPage from './routes/main';
 import LoginPage from './routes/login';
 import CreatePostPage from './routes/create-post';
 
-const getPosts = gql`
-  query Posts {
-    posts {
-      _id
-      date
-      tags {
-        _id
-        name
-      }
-      photos {
-        src
-        description
-      }
-      title
-      text
-      privacy
-    }
-  }
-`;
-
 const App = () => {
-  const { loading, error, data } = useQuery(getPosts);
-
-  if (error) {
-    return error;
-  }
-
-  if (loading) {
-    return 'Loading...';
-  }
-
-  if (data.posts.length === 0) {
-    return 'No posts yet.';
-  }
-
   return (
     <div className={commonStyles.pageContainer}>
       <PageHeader title={'tsvetta archive'} />
