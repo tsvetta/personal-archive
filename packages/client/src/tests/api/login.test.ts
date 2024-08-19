91d803dc6b71606dc56868e440165fbb699b43c1
tsvetta <tsvetta@gmail.com>
2024-08-19 13:29:24 +0400

    Tests: API - check token cookie

--- packages/client/src/tests/api/login.test.ts
+++ packages/client/src/tests/api/login.test.ts
@@ -12,6 +12,21 @@ import { createTestContext, TestContext } from '../entry-tests.js';
 const setJWTTokenCookieRegex =
   /(auth_token|refresh_token)=[\w-]+\.[\w-]+\.[\w-]+; Max-Age=\d+; Path=\/; Expires=.+; HttpOnly; SameSite=Strict/;
 
+const checkTokenCookie = (tokenCookie: string, userId: string) => {
+  expect(tokenCookie).toMatch(setJWTTokenCookieRegex);
+
+  const tokenMatch = tokenCookie.match(
+    /(auth_token|refresh_token)=([\w-]+\.[\w-]+\.[\w-]+)/
+  );
+  const token = tokenMatch && tokenMatch[2];
+  const tokenDecoded = jwt.decode(token || '');
+
+  expect(tokenDecoded).toHaveProperty('userId', userId);
+  expect(tokenDecoded).toHaveProperty('username', 'tsvetta');
+  expect(tokenDecoded).toHaveProperty('role', 'TSVETTA');
+  expect(tokenDecoded).toHaveProperty('accessLevel', 4);
+};
+
 describe('Authorization', () => {
   suite('Successful authorization', () => {
     let t: TestContext;
@@ -44,50 +59,22 @@ describe('Authorization', () => {
 
       expect(response.status).toBe(200);
 
+      expect(response.body.data.loginUser).toHaveProperty('_id');
       expect(response.body.data.loginUser).toHaveProperty(
         'username',
         'tsvetta'
       );
       expect(response.body.data.loginUser).toHaveProperty('role', 'TSVETTA');
       expect(response.body.data.loginUser).toHaveProperty('accessLevel', 4);
+      expect(response.body.data.loginUser).toHaveProperty('refreshToken');
 
-      // --- AUTH TOKEN
+      const { _id } = response.body.data.loginUser;
+
       const setAuthTokenCookie = response.headers['set-cookie'][0];
-      expect(setAuthTokenCookie).toMatch(setJWTTokenCookieRegex);
+      checkTokenCookie(setAuthTokenCookie, _id);
 
-      const authTokenMatch = setAuthTokenCookie.match(
-        /auth_token=([\w-]+\.[\w-]+\.[\w-]+)/
-      );
-      const authToken = authTokenMatch && authTokenMatch[1];
-      const authTokenDecoded = jwt.decode(authToken || '');
-
-      expect(authTokenDecoded).toHaveProperty(
-        'userId',
-        response.body.data.loginUser._id
-      );
-      expect(authTokenDecoded).toHaveProperty('username', 'tsvetta');
-      expect(authTokenDecoded).toHaveProperty('role', 'TSVETTA');
-      expect(authTokenDecoded).toHaveProperty('accessLevel', 4);
-
-      // --- REFRESH TOKEN
       const setRefreshTokenCookie = response.headers['set-cookie'][1];
-      expect(setRefreshTokenCookie).toMatch(setJWTTokenCookieRegex);
-
-      const refreshTokenMatch = setRefreshTokenCookie.match(
-        /refresh_token=([\w-]+\.[\w-]+\.[\w-]+)/
-      );
-      const refreshToken = refreshTokenMatch && refreshTokenMatch[1];
-      const refreshTokenDecoded = jwt.decode(refreshToken || '');
-
-      expect(refreshTokenDecoded).toHaveProperty(
-        'userId',
-        response.body.data.loginUser._id
-      );
-      expect(refreshTokenDecoded).toHaveProperty('username', 'tsvetta');
-      expect(refreshTokenDecoded).toHaveProperty('role', 'TSVETTA');
-      expect(refreshTokenDecoded).toHaveProperty('accessLevel', 4);
-
-      expect(refreshToken).toEqual(response.body.data.loginUser.refreshToken);
+      checkTokenCookie(setRefreshTokenCookie, _id);
     });
   });
 });
