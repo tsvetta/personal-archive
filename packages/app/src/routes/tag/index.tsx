1a7a4cedc6ffc51896e74b9e931590025c980fa2
tsvetta <tsvetta@gmail.com>
2024-10-12 23:30:34 +0400

    New Page: Tag page + grapql queries to app

---
+++ packages/app/src/routes/tag/index.tsx
@@ -0,0 +1,38 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { getPosts } from '../../apollo/queries.js';
+import Post, { PostData } from '../../components/Post/index.js';
+
+const TagPage = () => {
+  const { tagId } = useParams();
+
+  const { data, error, loading } = useQuery(getPosts, {
+    variables: { tagId },
+    skip: !tagId,
+  });
+
+  if (error) {
+    console.error('\n Main page error:', error);
+
+    return error.message;
+  }
+
+  if (loading) {
+    return 'Loading...';
+  }
+
+  if (!data || !data.posts || data.posts.length === 0) {
+    return 'No posts yet.';
+  }
+
+  return (
+    <article className='article'>
+      {data.posts.map((post: PostData) => {
+        return <Post key={post._id} data={post} />;
+      })}
+    </article>
+  );
+};
+
+export default TagPage;
