70ae6b4ff29a9e551f871fc653a71c81f0f9c5bc
tsvetta <tsvetta@gmail.com>
2024-10-12 23:30:19 +0400

    eslint: fix linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  settings: {
+    'import/resolver': {
+      // You will also need to install and configure the TypeScript resolver
+      // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
+      typescript: true,
+      node: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-cycle': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
