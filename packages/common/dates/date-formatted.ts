c7acb9c132553c8fb5d19eef45645f1a69725c8b
tsvetta <tsvetta@gmail.com>
2024-10-15 18:40:24 +0400

    remove console.log

--- packages/common/dates/date-formatted.ts
+++ packages/common/dates/date-formatted.ts
@@ -1,8 +1,6 @@
 export const getDateFormatted = (date?: number | Date) => {
   const now = date ? new Date(date) : new Date();
 
-  console.log(222, now);
-
   const d = ('0' + now.getDate()).slice(-2);
   const m = ('0' + (now.getMonth() + 1)).slice(-2);
   const y = now.getFullYear();
